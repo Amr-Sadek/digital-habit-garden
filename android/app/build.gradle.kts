cf9d06821d987da361a9251f64f1600d370a043c
amr-sadek <amrsadek510@gmail.com>
2026-08-23 03:37:56 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,6 +42,13 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+    androidComponents {
+        onVariants(selector().withBuildType("release")) { variant ->
+            variant.outputs.forEach { output ->
+                output.outputFileName.set("Digital_Habit_Garden_${flutter.versionName}.apk")
+            }
+        }
+    }
 }
 
 dependencies {
